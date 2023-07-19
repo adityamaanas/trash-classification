16ceae9dc40eda32c52d8df8be8069f15bd8bfc2
adityamaanas <64123559+adityamaanas@users.noreply.github.com>
2023-07-19 16:05:07 +0400

    Update main.py to directly reference exported model on github

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     allow_headers=headers,
 )
 
-learn = load_learner("C:/Users/adipi/Documents/Code/Internship-RAKRIC-0723/Original/export.pkl")
+learn = load_learner("https://github.com/adityamaanas/trash-classification/blob/main/export.pkl")
 
 
 @app.get("/")
