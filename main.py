6b4d0dc3aa59097bf25451b16224c08bb5b1dbca
adityamaanas <adityamaanas2@gmail.com>
2023-07-24 19:12:24 +0400

    main.py supressed wildcard import warning+gitignore

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from uvicorn import run
-from fastai.vision.all import *
+from fastai.vision.all import * # type: ignore
 import skimage
 from urllib.request import urlopen
 import os
