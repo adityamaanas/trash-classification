d4a948da3e0de258399983646d49091c76c186da
adityamaanas <adityamaanas2@gmail.com>
2023-07-26 12:11:31 +0400

    working cropping

--- croppingbox.py
+++ croppingbox.py
@@ -1,25 +1,32 @@
 from ultralytics import YOLO
 import cv2
 from ROI import get_box
+from urllib.request import urlopen
+from fastai.vision.all import * # type: ignore
 
-model = YOLO('yolov8n-seg.pt')  # load an official model
-img_url = 'https://ultralytics.com/images/bus.jpg'
-img = cv2.imread("C:/Users/adipi/Documents/Code/Internship-RAKRIC-0723/trash-classification/bus.jpg")
+model = YOLO('yolov8n-seg.pt')
 
-boxes = get_box(model, img_url)
-boxes = boxes.xyxy
+def crop(img_url, img):
+    cropped = []
+    boxes = get_box(model, img_url)
+    boxes = boxes.xyxy
+    #img = PILImage.create(urlopen(img_url))
+    #img = np.array(img)
 
-# create separate images for each box
-for box in boxes:
-    # get the coordinates of the box
-    x1 = int(box[0])
-    y1 = int(box[1])
-    x2 = int(box[2])
-    y2 = int(box[3])
+    # create separate images for each box
+    for box in boxes:
+        # get the coordinates of the box
+        x1 = int(box[0])
+        y1 = int(box[1])
+        x2 = int(box[2])
+        y2 = int(box[3])
 
-    # crop the image
-    crop_img = img[y1:y2, x1:x2]
+        # crop the image
+        crop_img = img[y1:y2, x1:x2]
+        cropped.append(crop_img)
 
-    # show the cropped image
-    cv2.imshow("cropped", crop_img)
-    cv2.waitKey(0)
+        # show the cropped image
+        #cv2.imshow("cropped", crop_img)
+        #cv2.waitKey(0)
+
+    return cropped # type: ignore
